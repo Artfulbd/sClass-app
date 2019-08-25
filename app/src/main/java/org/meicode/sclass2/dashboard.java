a31b62001a54635168720bd558c5be375cece99a
HM Tamim <hm.tamim@northsouth.edu>
2019-08-25 21:06:27 +0600

    dashboard

--- app/src/main/java/org/meicode/sclass2/dashboard.java
+++ app/src/main/java/org/meicode/sclass2/dashboard.java
@@ -2,28 +2,39 @@ package org.meicode.sclass2;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +43,16 @@ public class dashboard extends AppCompatActivity implements NavigationView.OnNav
     private static TextView res;
     private Button logout;
     UserDetails user;
+
+
+
+
+    ArrayList<ClassListItem> items;
+    ClassListAdapter adapter;
+    RecyclerView recyclerView;
+    private RequestQueue mQueue;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +77,175 @@ public class dashboard extends AppCompatActivity implements NavigationView.OnNav
         toggle.syncState();
 
 
+
+
+
+
+
+        mQueue = Volley.newRequestQueue(this);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager manager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+        items=new ArrayList<>();
+        adapter=new ClassListAdapter(this, items);
+        recyclerView.setAdapter(adapter);
+
+
+
+        adapter.notifyDataSetChanged();
+
+
+        // it will load data from api and show on recyclerview
+
+        // getUserData();
+
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                addCourseDialog();
+
+            }
+        });
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+    private void addCourseDialog(){
+
+
+
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(dashboard.this);
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alert_add_course, null);
+        dialogBuilder.setView(dialogView);
+
+        AlertDialog alertDialog = dialogBuilder.create();
+
+        TextView courseNameView = dialogView.findViewById(R.id.courseName);
+        Button d_submit = dialogView.findViewById(R.id.submit);
+
+
+        alertDialog.getWindow()
+                .setLayout(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+
+        alertDialog.show();
+
+
+        d_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                // call API
+
+//                addCourseAPICall(
+//                        serDetails.getUserID(),
+//                        courseNameView.getText().toString()
+//                );
+
+
+
+            }
+        });
+
+
+
+    }
+
+
+
+    public  void getUserData(String pass, String id){
+        //final Intent dash = new Intent(this, dashboard.class);
+        mQueue = Volley.newRequestQueue(this);
+        Map<String, String> params = new HashMap<>();
+
+        params.put("id",id);
+        params.put("pass", pass);
+
+        JSONObject payload = new JSONObject(params);
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, "http://url of user info", payload, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+
+                    Log.d("json response", response.toString());
+                    if(response.getString("status").equals("Ok")){
+
+                        JSONArray jsonArray = response.getJSONArray("classList");
+
+                        for (int i=0; i < jsonArray.length(); i++){
+
+
+                            items.add(new ClassListItem(
+                                    jsonArray.getJSONObject(i).getString("classid"),
+                                    jsonArray.getJSONObject(i).getString("classname")
+                            ));
+
+                        }
+
+
+                        adapter.notifyDataSetChanged();
+
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //user.setErrorMassege("Connection error");
+            }
+        });
+        mQueue.add(req);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
